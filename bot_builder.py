5514f4bc2193cb816d45f8d8a4a47b05642607dc
Gabriel H. Silva <42482192+TheSmileyDroid@users.noreply.github.com>
2022-10-12 23:30:21 +0000

    replit fix

--- bot_builder.py
+++ bot_builder.py
@@ -1,21 +1,24 @@
 from typing import Type
 import discord
 from discord.ext import commands
-
+import asyncio
 
 class BotBuilder():
 
     def __init__(self):
-        self.bot: commands.Bot = commands.Bot(command_prefix="-",
+      intents = discord.Intents.all()
+      self.bot: commands.Bot = commands.Bot(command_prefix="-",
                                               strip_after_prefix=True,
-                                              case_insensitive=True)
+                                              case_insensitive=True, intents=intents)
 
     def add_cog(self, cog: Type[commands.Cog]):
-        self.bot.add_cog(cog(self.bot))
+      loop = asyncio.get_event_loop()
+      coroutine = self.bot.add_cog(cog(self.bot))
+      loop.run_until_complete(coroutine)
 
     def remove_cog(self, cog_name: str):
-        self.bot.remove_cog(cog_name)
+      self.bot.remove_cog(cog_name)
 
     def run(self, idx: str):
-        self.bot.run(idx)
+      self.bot.run(idx)
         
--- smiley_bot.py
+++ smiley_bot.py
@@ -1,7 +1,11 @@
 import os
+
 from bot_builder import BotBuilder
 from Cogs import (default_cog, tocar_audio_cog, reddit_cog, falar_cog, rpg_cog)
 from app import keep_alive
+import discord
+
+discord.opus.load_opus('./libopus.so.0.8.0')
 
 bot = BotBuilder()
 
@@ -10,7 +14,6 @@ if __name__ == '__main__':
     bot.add_cog(default_cog.Basic)
     bot.add_cog(falar_cog.Falar)
     bot.add_cog(tocar_audio_cog.Audio)
-    bot.add_cog(reddit_cog.Reddit)
     bot.add_cog(rpg_cog.RPG)
 
     # Keep 
